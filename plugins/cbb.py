9307b382622cb57846c7ced86f6e256a79c20450
Md Siam <130592253+Mdsiam13@users.noreply.github.com>
2023-07-08 12:18:44 +0600

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -1,1 +1,28 @@
+#(©)Codexbotz
 
+from pyrogram import __version__
+from bot import Bot
+from config import OWNER_ID
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+
+@Bot.on_callback_query()
+async def cb_handler(client: Bot, query: CallbackQuery):
+    data = query.data
+    if data == "about":
+        await query.message.edit_text(
+            text = f"<b>○ Creator : This Person</a>\n○ Contact Me : <a href='https://www.facebook.com/profile.php?id=100092142554892&mibextid=ZbWKwL'>Facebook</a>\n○ Follow Us : <a href='https://instagram.com/anime_onec?igshid=MzNlNGNkZWQ4Mg=='>Instragram</a>\n○ Channel : <a href='https://t.me/Anime_Apple'>Anime Apple</a>\n○ Support Group : @AniChat_Box</b>",
+            disable_web_page_preview = True,
+            reply_markup = InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("❌ Close", callback_data = "close")
+                    ]
+                ]
+            )
+        )
+    elif data == "close":
+        await query.message.delete()
+        try:
+            await query.message.reply_to_message.delete()
+        except:
+            pass
